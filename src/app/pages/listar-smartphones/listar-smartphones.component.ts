51b5234712650bd585bf74af60e0066fa5eb1b09
LuisManuel <jimenezhernandezluismanuel3g@gmail.com>
2024-06-30 17:32:09 -0600

    Se crea el código listar-smartphones

--- src/app/pages/listar-smartphones/listar-smartphones.component.ts
+++ src/app/pages/listar-smartphones/listar-smartphones.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Smartphone } from '../../models/smartphone';
+import { Component, OnInit } from '@angular/core';
+import { Smartphone } from '../../models/Smartphone';
 import { SmartphoneService } from '../../services/smartphone.service';
 
 @Component({
@@ -7,24 +7,31 @@ import { SmartphoneService } from '../../services/smartphone.service';
   templateUrl: './listar-smartphones.component.html',
   styleUrl: './listar-smartphones.component.css'
 })
-export class ListarSmartphonesComponent {
+export class ListarSmartphonesComponent  implements OnInit{
 
-  listaSmartphones: Smartphone[] = [];
+  smartphones:any []= [];
 
-  constructor(
-    private smartphoneService: SmartphoneService
-  ) {}
+  constructor( private smartphoneService: SmartphoneService) {}
 
+  //Obtener Smartphone desde el OnInit
   ngOnInit() {
     this.smartphoneService.getSmartphones()
-    .subscribe(
-      // Correct data
-      data => {
-        console.log(data);
-      },
-    );
-
-
+    .subscribe((data:any)=>{
+      this.smartphones=data;
+    });
+    
   }
 
+  
+
+
+  //Método para eliminar Smartphone
+  eliminarSmartphone(smartphone: any, index:number){
+    if(window.confirm("¿Seguro que desea Eliminar el Smartphone?")){
+      this.smartphoneService.deleteSmartphone(smartphone._id)
+      .subscribe((data)=>{
+        this.smartphones.splice(index,1)
+      })
+    }
+  }
 }
