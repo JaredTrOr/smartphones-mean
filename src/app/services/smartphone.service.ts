74af6b057c4a50a853ba076269aa4ba1d8c3f512
AlanAZ01 <alan.acevedo.zarraga@gmail.com>
2024-06-30 14:00:40 -0600

    Creación de Empleado Service

--- src/app/services/smartphone.service.ts
+++ src/app/services/smartphone.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, map, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SmartphoneService {
 
-  constructor() { }
+  baseUrl: string = 'http:localhost:4000/api';
+  headers = new HttpHeaders().set('Content-Type', 'aplication/json');
+
+  constructor(private http:HttpClient) { }
+
+  agregarSmartphone(data):Observable<any>{
+    let url = `${this.baseUrl}/agregar`;
+    return this.http.post(url,data).pipe(catchError(this.errorManager));
+  }
 }
