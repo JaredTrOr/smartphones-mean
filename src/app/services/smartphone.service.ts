4755a85271711ecbc9acc4b4691a5a88b570d95b
AlanAZ01 <alan.acevedo.zarraga@gmail.com>
2024-06-30 22:36:07 -0600

    Se agregó la baseUri y se cambiaron los enpoints

--- src/app/services/smartphone.service.ts
+++ src/app/services/smartphone.service.ts
@@ -9,7 +9,7 @@ import { Observable, catchError, throwError } from 'rxjs';
 })
 export class SmartphoneService {
 
-  baseUri:string='';
+  baseUri:string='https://backend-smartphones-deploy.onrender.com/api';
 
   headers = new HttpHeaders()
   .set('Content-type','application/json');
@@ -18,33 +18,33 @@ export class SmartphoneService {
 
   //Método para agregar Smartphone
   addSmartphone(data:Smartphone): Observable<any>{
-    let url=`${this.baseUri}/add`;
+    let url=`${this.baseUri}/create-smartphone`;
     return  this.http.post(url,data)
     .pipe(catchError(this.errorManager))
   }
 
   //Método para obtener todos los Smartphones
   getSmartphones(){
-    let url=`${this.baseUri}/smatphones`;
+    let url=`${this.baseUri}/get-smatphones`;
     return this.http.get(url);
   }
 
    //Método para obtener Smartphone por id
   getSmartphone(id: string): Observable<Smartphone> {
-    let url=`${this.baseUri}/get/${id}`;
+    let url=`${this.baseUri}/get-smartphone-id/${id}`;
     return this.http.get<Smartphone>(url)
   }
 
 
   //Método para actualizar
   updateSmartphone(id: string, data: Smartphone): Observable<Smartphone>{
-    let url = `${this.baseUri}/update/${id}`;
+    let url = `${this.baseUri}/update-smartphone/${id}`;
     return this.http.put<Smartphone>(url,data);
   }
 
   //Método para eliminar Smartphone
   deleteSmartphone(id: string): Observable<any>{
-    let url = `${this.baseUri}/delete/${id}`;
+    let url = `${this.baseUri}/delete-smartphone/${id}`;
     return this.http.delete<Smartphone>(url);
   }
 
